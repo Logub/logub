0d4bc03e871938e4877a640c99c18be16e24cf85
Alexis Gardin <alexisgardin@gmail.com>
2021-05-08 17:25:51 +0200

    remove useless file

--- frontend/.nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-import { wrapFunctional } from './utils'
-
-export { default as NavigationLogubDrawer } from '../../components/navigation/LogubDrawer.vue'
-export { default as LogViewer } from '../../components/log-viewer/LogViewer.vue'
-export { default as LogViewerSearchBar } from '../../components/log-viewer/SearchBar.vue'
-
-export const LazyNavigationLogubDrawer = import('../../components/navigation/LogubDrawer.vue' /* webpackChunkName: "components/navigation-logub-drawer" */).then(c => wrapFunctional(c.default || c))
-export const LazyLogViewer = import('../../components/log-viewer/LogViewer.vue' /* webpackChunkName: "components/log-viewer" */).then(c => wrapFunctional(c.default || c))
-export const LazyLogViewerSearchBar = import('../../components/log-viewer/SearchBar.vue' /* webpackChunkName: "components/log-viewer-search-bar" */).then(c => wrapFunctional(c.default || c))
