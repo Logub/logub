b6fb4c7e5d0fa0beabb3a77ea5faa9647dc539d3
Florian Juroszek <fjuroszek@laprovence.com>
2021-05-12 00:01:55 +0200

    Keep only quote generator

--- generator/java/log-generator/src/main/java/com/loghub/loggenerator/LogGenerator.java
+++ generator/java/log-generator/src/main/java/com/loghub/loggenerator/LogGenerator.java
@@ -12,35 +12,35 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class LogGenerator {
-  Logger logger = LoggerFactory.getLogger(LogGenerator.class);
-  @Autowired
-  private QuoteGeneratorService quoteGeneratorService;
+    Logger logger = LoggerFactory.getLogger(LogGenerator.class);
+    @Autowired
+    private QuoteGeneratorService quoteGeneratorService;
 
-  @Scheduled(fixedDelay = 1)
-  public void generateLog(){
+    @Scheduled(fixedDelay = 1)
+    public void generateLog() {
+        quoteGeneratorService.generate();
 
-    MDC.put("test", "test");
-    MDC.put("businessValue", "test");
-    Random random = new Random();
-      String generatedString = generateRandomString(50);
-      logger.info(generatedString);
-      quoteGeneratorService.generate();
+        Random random = new Random();
+        try {
+            TimeUnit.MILLISECONDS.sleep(random.nextInt(1500));
+        } catch (InterruptedException e) {
+            logger.error("An error occurred while generating a log");
+        }
+    }
 
-      try {
-        TimeUnit.MILLISECONDS.sleep(random.nextInt(1500));
-      } catch (InterruptedException e) {
-        logger.error(generatedString);
-      }
-  }
+    private String generateRandomString(final int stringLength) {
+        MDC.put("test", "test");
+        MDC.put("businessValue", "test");
+        String generatedString = generateRandomString(50);
+        logger.info(generatedString);
 
-  private String generateRandomString(final int stringLength) {
-    Random random = new Random();
+        Random random = new Random();
 
-    // 97 is for 'a' and 122 for 'z'
-    return random.ints(97, 122 + 1)
-        .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-        .limit(stringLength)
-        .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-        .toString();
-  }
+        // 97 is for 'a' and 122 for 'z'
+        return random.ints(97, 122 + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(stringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
 }
